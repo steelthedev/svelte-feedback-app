cbbcf5e1edff5c6d3a3f4d107939ae02ec25b29e
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-08-25 15:51:01 +0100

    added project

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,23 @@
+<script>
+    // import { onMount, onDestroy } from 'svelte';
+    import {fade, scale} from 'svelte/transition'
+    import FeedbackItem from "./FeedbackItem.svelte";
+    import {FeedbackStore} from '../stores'
+
+//     let feedback = []
+//    const unsubscribe =  FeedbackStore.subscribe((data) => feedback = data)
+
+//     onMount(() =>{
+
+//     })
+
+//     onDestroy(() => {
+//         unsubscribe()
+//     })
+</script>
+
+{#each $FeedbackStore as fb (fb.id) }
+    <div in:scale out:fade>
+        <FeedbackItem item={fb}/>
+    </div>
+{/each}
